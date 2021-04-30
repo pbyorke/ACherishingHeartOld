3576275863fe75472bf476fd4bb493a88f4218e9
Pete Yorke <pbyorke@gmail.com>
2021-04-30 12:10:49 -0400

    Initial Commit

---
+++ Shared/ACherishingHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACherishingHeartApp.swift
+//  Shared
+//
+//  Created by Pete Yorke on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct ACherishingHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
